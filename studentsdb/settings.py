b91952620277ca16451ae72eda7818354cbeb150
Alex Bukhonenko <alex.bukhonenko@gmail.com>
2015-09-22 00:51:19 +0300

    custom context_processors

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "studentsdb.context_processors.students_proc",
                 # 'django.core.context_processors.request',
             ],
         },
@@ -90,7 +91,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
@@ -103,3 +104,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PORTAL_URL = ''
