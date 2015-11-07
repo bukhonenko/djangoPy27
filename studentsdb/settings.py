94d7c9f92302ddb3bcdeab187f4511fc7017fdc9
Alex Bukhonenko <alex.bukhonenko@gmail.com>
2015-11-07 19:24:05 +0200

    admin form upgraded

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'endless_pagination',
+    #'endless_pagination',
     'crispy_forms',
     'students',
 )
